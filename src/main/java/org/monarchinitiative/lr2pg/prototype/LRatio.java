b41f6416458864a1a19a9c121a738d5249b9871c
ravanv <vidarmehr@gmail.com>
2017-08-29 16:43:37 -0400

    Likelihood ratio

--- src/main/java/org/monarchinitiative/lr2pg/prototype/LRatio.java
+++ src/main/java/org/monarchinitiative/lr2pg/prototype/LRatio.java
@@ -4,14 +4,14 @@ import org.apache.log4j.Logger;
 
 public class LRatio {
     static Logger logger = Logger.getLogger(LRatio.class.getName());
-    private int x,y;
+  //  private int x,y;
 
     /**
      *
-     * @param pretestprob
-     * @param b
+     //* @param pretestprob
+     //* @param b
      */
-    LRatio(int pretestprob, int b) {
+   /* LRatio(int pretestprob, int b) {
         this.x=pretestprob;
         this.y=b;
         logger.trace(String.format("x=%d,y=%d",x,y ));
@@ -22,7 +22,26 @@ public class LRatio {
 
     public double odds() {
         return -1d;
+    }*/
+    private double x,y,z;
+    LRatio(double pretestprob, double sensit, double specif) {
+        this.x = pretestprob;
+        this.y = sensit;
+        this.z = specif;
+       // logger.trace(String.format("x=%f,y=%f,z=%f",x,y,z ));
     }
+
+    public double ratio() {return y/(1-z);}
+
+    public double pretestodds() {return x/(1-x);}
+
+    //public double posttestodds(){return (x/(1-x))*(y/(1-z));}
+
+    //public double posttestprob(){return ((x/(1-x))*(y/(1-z)))/(1+(x/(1-x))*(y/(1-z)));}
+
+
+
+
 
 
 
--- src/test/java/org/monarchinitiative/lr2pg/prototype/LRatioTest.java
+++ src/test/java/org/monarchinitiative/lr2pg/prototype/LRatioTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class LRatioTest {
 
 
-    @Test
+   /* @Test
     public void testSum() {
         int x=10;
         int y=12;
@@ -20,8 +20,64 @@ public class LRatioTest {
     public void testOdds() {
         LRatio lr = new LRatio(2,3);
         double expected= 0.03; /* pretest odds */
-        double epsilon=0.01;
+       /* double epsilon=0.01;
         Assert.assertEquals(expected,lr.odds(),epsilon);
+    }*/
+
+/* Added by Vida*/
+
+    @Test
+    public void testRatio(){
+        double pretesprob = 0.01;
+        double sensit = 0.4;
+        double specif = 0.9;
+        double epsilon = 0.001;
+        LRatio lr = new LRatio(pretesprob,sensit,specif);
+        Assert.assertEquals(4,lr.ratio(),epsilon);
+    }
+
+    @Test
+    public void testPretestOdds(){
+        double pretesprob = 0.01;
+        double sensit = 0.4;
+        double specif = 0.9;
+        double epsilon = 0.001;
+        LRatio lr = new LRatio(pretesprob,sensit,specif);
+        Assert.assertEquals(0.0101,lr.pretestodds(),epsilon);
+    }
+    @Test
+    public void testPosttestOdds(){
+        double pretesprob = 0.01;
+        double sensit = 0.4;
+        double specif = 0.9;
+        double epsilon = 0.001;
+        LRatio lr = new LRatio(pretesprob,sensit,specif);
+        Assert.assertEquals(0.040,lr.ratio()*lr.pretestodds(),epsilon);
+    }
+
+    @Test
+    public void testPosttestProb() {
+        double pretesprob = 0.01;
+        double sensit = 0.4;
+        double specif = 0.9;
+        double epsilon = 0.001;
+        LRatio lr = new LRatio(pretesprob, sensit, specif);
+        Assert.assertEquals(0.038, (lr.ratio()*lr.pretestodds())/(1+lr.ratio()*lr.pretestodds()), epsilon);
+    }
+    /* all above tests in one test*/
+     @Test
+     public void testlikelihoodratio(){
+            double pretesprob = 0.01;
+            double sensit = 0.4;
+            double specif = 0.9;
+            double epsilon = 0.001;
+            LRatio lr = new LRatio(pretesprob,sensit,specif);
+            Assert.assertEquals(4,lr.ratio(),epsilon);
+            Assert.assertEquals(0.0101,lr.pretestodds(),epsilon);
+            Assert.assertEquals(0.04,lr.ratio()*lr.pretestodds(),epsilon);
+            Assert.assertEquals(0.038, (lr.ratio()*lr.pretestodds())/(1+lr.ratio()*lr.pretestodds()), epsilon);
+           // Assert.assertEquals(0.44,lr.posttestodds(),epsilon);
+           // Assert.assertEquals(0.30, lr.posttestprob(), epsilon);
     }
 
 }
