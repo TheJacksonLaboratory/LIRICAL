4f55e0144cab7b4775d9033b824879344ada630e
pnrobinson <peter.robinson@charite.de>
2018-06-17 17:36:05 -0400

    documentation

---
+++ src/main/java/org/monarchinitiative/lr2pg/hpo/HpoPhenoGenoCaseSimulator.java
@@ -0,0 +1,8 @@
+package org.monarchinitiative.lr2pg.hpo;
+
+/**
+ * This class is intended to try out some architectures for the genotype-phenotype
+ * LR test. Much of the code is copied from {@link HpoCaseSimulator}
+ */
+public class HpoPhenoGenoCaseSimulator {
+}
