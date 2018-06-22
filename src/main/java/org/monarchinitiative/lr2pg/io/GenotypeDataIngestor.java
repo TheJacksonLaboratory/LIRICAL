89fd484419454b170de43497809e952f232f5573
pnrobinson <peter.robinson@charite.de>
2018-06-22 09:19:06 -0400

    cleanup

--- src/main/java/org/monarchinitiative/lr2pg/io/GenotypeDataIngestor.java
+++ src/main/java/org/monarchinitiative/lr2pg/io/GenotypeDataIngestor.java
@@ -43,9 +43,9 @@ public class GenotypeDataIngestor {
                     continue; // no EntrezId available -- this happens with many genes
                 }
                 TermId entrezId=new TermId(ENTREZ_GENE_PREFIX,entrezNumber);
-                String fsumpath=a[9];
+                String fsumpath=a[5];
                 try {
-                    Double pathSum = Double.parseDouble(a[9]);
+                    Double pathSum = Double.parseDouble(fsumpath);
                     //builder.put(entrezId,pathSum);
                     gene2freq.put(entrezId,pathSum);
                 } catch (NumberFormatException e) {
