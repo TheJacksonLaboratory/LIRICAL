3e6e9776b6b5bca8ce5a061fb58ee38f422ec256
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-08-20 15:25:52 +0100

    Wrap SimulateCaseCommand BufferedWriter in try-with-resources

--- src/main/java/org/monarchinitiative/lr2pg/command/SimulateCasesCommand.java
+++ src/main/java/org/monarchinitiative/lr2pg/command/SimulateCasesCommand.java
@@ -43,7 +43,7 @@ public class SimulateCasesCommand implements Command {
     }
 
     public void execute() {
-        logger.error("Executing HpoCase simulation");
+        logger.info("Executing HpoCase simulation");
         if (gridSearch) {
             try {
                 gridsearch();
@@ -72,61 +72,64 @@ public class SimulateCasesCommand implements Command {
         int[] termnumber = {1,2,3,4,5,6,7,8,9,10};
         int[] randomtermnumber = {0,1,2,3,4,0,1,2,3,4};
         String outfilename="gridsearch.R";
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outfilename));
-        double[][] Z = new double[termnumber.length][randomtermnumber.length];
-        int n_cases=100;
-        PhenotypeOnlyHpoCaseSimulator simulator;
-        for (int i=0;i<termnumber.length;i++) {
-            for (int j=0;j<randomtermnumber.length;j++) {
-                boolean imprec = (j>4);
-                simulator = new PhenotypeOnlyHpoCaseSimulator(this.dataDirectoryPath,n_cases, termnumber[i], randomtermnumber[j],imprec);
-                simulator.simulateCases();
-                Z[i][j] = simulator.getProportionAtRank1();
-                writer.write(String.format("terms: %d; noise terms: %d; percentage at rank 1: %.2f\n",
-                        termnumber[i],
-                        randomtermnumber[j],
-                        100.0* Z[i][j]));
+        double[][] Z;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outfilename))) {
+            Z = new double[termnumber.length][randomtermnumber.length];
+            int n_cases = 100;
+            PhenotypeOnlyHpoCaseSimulator simulator;
+            for (int i = 0; i < termnumber.length; i++) {
+                for (int j = 0; j < randomtermnumber.length; j++) {
+                    boolean imprec = (j > 4);
+                    simulator = new PhenotypeOnlyHpoCaseSimulator(this.dataDirectoryPath, n_cases, termnumber[i], randomtermnumber[j], imprec);
+                    simulator.simulateCases();
+                    Z[i][j] = simulator.getProportionAtRank1();
+                    writer.write(String.format("terms: %d; noise terms: %d; percentage at rank 1: %.2f\n",
+                            termnumber[i],
+                            randomtermnumber[j],
+                            100.0 * Z[i][j]));
+                }
             }
-        }
-        String xstring=String.format("X <- c(%s)\n",
-                Arrays.stream(termnumber).
-                        mapToObj(Integer::toString)
-                        .collect(Collectors.joining(", ")));
-        String ystring=String.format("Y <- c(%s)\n",
-                Arrays.stream(randomtermnumber).
-                        mapToObj(Integer::toString)
-                        .collect(Collectors.joining(", ")));
-        List<String> Zlist = new ArrayList<>();
-        for (int i = 0; i < termnumber.length; i++) {
-            for (int j=0;j<randomtermnumber.length;j++) {
-                Zlist.add(String.format("%.2f",100.0*Z[i][j]));
+            String xstring = String.format("X <- c(%s)\n",
+                    Arrays.stream(termnumber).
+                            mapToObj(Integer::toString)
+                            .collect(Collectors.joining(", ")));
+            String ystring = String.format("Y <- c(%s)\n",
+                    Arrays.stream(randomtermnumber).
+                            mapToObj(Integer::toString)
+                            .collect(Collectors.joining(", ")));
+            List<String> Zlist = new ArrayList<>();
+            for (int i = 0; i < termnumber.length; i++) {
+                for (int j = 0; j < randomtermnumber.length; j++) {
+                    Zlist.add(String.format("%.2f", 100.0 * Z[i][j]));
+                }
             }
+            String zstring = String.format("Z <- matrix(c(%s),nrow=%d,ncol=%d)\n",
+                    Zlist.stream().collect(Collectors.joining(",")),
+                    termnumber.length,
+                    randomtermnumber.length);
+            writer.write("library(plot3D)\n");
+            writer.write(xstring + ystring + zstring);
+
+            String xlab = String.format("c(%s)\n",
+                    Arrays.stream(termnumber).
+                            mapToObj(i -> String.format("\"%d\"", i))
+                            .collect(Collectors.joining(", ")));
+            String create3d = String.format(" hist3D(X,Y,Z,xlab=%s, clab=\"%% at rank 1\",zlim=c(0,1))\n", xlab);
+            writer.write(create3d);
+            //alternatively, do a grouped bar plot
+
+            String colnames = Arrays.stream(termnumber)
+                    .mapToObj(i -> String.format("\"%s\"", i))
+                    .collect(Collectors.joining(", "));
+            String rownames = Arrays.stream(randomtermnumber)
+                    .mapToObj(i -> String.format("\"%s\"", i))
+                    .collect(Collectors.joining(", "));
+
+            String barplotLegend = String.format("colnames(Z)=c(%s)\nrownames(Z)=c(%s)\n", colnames, rownames);
+            String barplot = "barplot(Z, col=colors()[30:32], border=\"white\", font.axis=2, beside=T, " +
+                    "legend=rownames(Z), xlab=\"Number of terms\", font.lab=2, cex.lab=2,cex.axis=1.5)\n";
+            writer.write(barplotLegend + barplot);
         }
-        String zstring=String.format("Z <- matrix(c(%s),nrow=%d,ncol=%d)\n",
-                Zlist.stream().collect(Collectors.joining(",")),
-                termnumber.length,
-                randomtermnumber.length);
-        writer.write("library(plot3D)\n");
-        writer.write(xstring + ystring +  zstring);
-
-        String xlab = String.format("c(%s)\n",
-                Arrays.stream(termnumber).
-                        mapToObj(i -> String.format("\"%d\"",i))
-                        .collect(Collectors.joining(", ")));
-        String create3d = String.format(" hist3D(X,Y,Z,xlab=%s, clab=\"%% at rank 1\",zlim=c(0,1))\n",xlab);
-        writer.write(create3d);
-        //alternatively, do a grouped bar plot
-
-        String colnames= Arrays.stream(termnumber).mapToObj(i->String.format("\"%s\"",i)).collect(Collectors.joining(", "));
-        String rownames= Arrays.stream(randomtermnumber).mapToObj(i->String.format("\"%s\"",i)).collect(Collectors.joining(", "));
-
-        String barplotLegend = String.format("colnames(Z)=c(%s)\nrownames(Z)=c(%s)\n",colnames,rownames);
-        String barplot = "barplot(Z, col=colors()[30:32], border=\"white\", font.axis=2, beside=T, " +
-                "legend=rownames(Z), xlab=\"Number of terms\", font.lab=2, cex.lab=2,cex.axis=1.5)\n";
-        writer.write(barplotLegend+barplot);
-
-
-        writer.close();
         for (int i=0;i<termnumber.length;i++) {
             for (int j=0;j<randomtermnumber.length;j++) {
                 System.err.println(String.format("terms: %d; noise terms: %d; percentage at rank 1: %.2f",
