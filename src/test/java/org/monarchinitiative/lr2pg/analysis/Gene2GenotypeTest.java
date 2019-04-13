14ea27dd34412599e1e79695ae528b7db9debb6f
pnrobinson <peter.robinson@jax.org>
2019-04-13 13:58:40 -0400

    Adding unit tests

--- src/test/java/org/monarchinitiative/lr2pg/analysis/Gene2GenotypeTest.java
+++ src/test/java/org/monarchinitiative/lr2pg/analysis/Gene2GenotypeTest.java
@@ -1,7 +1,9 @@
 package org.monarchinitiative.lr2pg.analysis;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.monarchinitiative.exomiser.core.model.TranscriptAnnotation;
 import org.monarchinitiative.exomiser.core.model.pathogenicity.ClinVarData;
 import org.monarchinitiative.phenol.ontology.data.TermId;
@@ -10,10 +12,29 @@ import org.monarchinitiative.phenol.ontology.data.TermId;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.monarchinitiative.lr2pg.analysis.Gene2Genotype.NO_IDENTIFIED_VARIANT;
 
 class Gene2GenotypeTest {
 
     private final static double EPSILON=0.000001;
+    private static Gene2Genotype nrasVar;
+
+    @BeforeAll
+    static void init() {
+        String symbol="NRAS";
+        TermId geneId = TermId.of("NCBIGene:4893");
+        nrasVar = new Gene2Genotype(geneId,symbol);
+        // The following annotation is completely made up, but don't worry it has the right syntax
+        TranscriptAnnotation annot = Mockito.mock(TranscriptAnnotation.class);
+        when(annot.toString()).thenReturn("uc001lfg.4: c.518A>C p.(E173A) MISSENSE_VARIANT");
+        List<TranscriptAnnotation> annotList = ImmutableList.of(annot);
+        float pathscore=0.95f;
+        float frequency=0.001f;
+        nrasVar.addVariant(1,114713908, "A","G",annotList,"0/1",pathscore,frequency, ClinVarData.ClinSig.NOT_PROVIDED);
+    }
 
     /**
      * Test calculation of pathogenicity score. This depends on the Exomiser
@@ -81,6 +102,60 @@ class Gene2GenotypeTest {
         float expected=0.0f;
         g2g.addVariant(1,114713908, "A","G",emptyList,"0/1",pathscore,frequency, ClinVarData.ClinSig.NOT_PROVIDED);
         assertEquals(expected,g2g.getSumOfPathBinScores(),EPSILON);
+    }
+
+
+    @Test
+    void testNoVarPathogenicity(){
+        Gene2Genotype g2g = NO_IDENTIFIED_VARIANT;
+        assertEquals(0,g2g.getSumOfPathBinScores(),EPSILON);
+        assertFalse(g2g.hasPredictedPathogenicVar());
+    }
+
+    /** Test that we can retrieve ClinVar pathogenic variants, and a few other simple getters.*/
+    @Test
+    void testClinvarCount() {
+        String symbol="NRAS";
+        TermId geneId = TermId.of("NCBIGene:4893");
+        Gene2Genotype g2g = new Gene2Genotype(geneId,symbol);
+        List<TranscriptAnnotation> emptyList = ImmutableList.of();
+        float pathscore=1.0f;
+        float frequency=0.01f;
+        g2g.addVariant(1,114713908, "A","G",emptyList,"0/1",pathscore,frequency, ClinVarData.ClinSig.PATHOGENIC);
+        assertTrue(g2g.hasPathogenicClinvarVar());
+        assertEquals(1,g2g.pathogenicClinVarCount());
+        g2g.addVariant(14,114713908, "A","G",emptyList,"0/1",pathscore,frequency, ClinVarData.ClinSig.PATHOGENIC);
+        assertEquals(2,g2g.pathogenicClinVarCount());
+        assertTrue(g2g.hasPredictedPathogenicVar());
+        assertEquals("NRAS",g2g.getSymbol());
+        assertEquals(geneId,g2g.getGeneId());
+    }
+
+    @Test
+    void testVarList() {
+        assertTrue(NO_IDENTIFIED_VARIANT.getVarList().isEmpty());
+        assertEquals(1,nrasVar.getVarList().size());
+    }
+
+    @Test
+    void testHomozygousPathogenicVar() {
+        String symbol="ATP6V0A4";
+        TermId geneId = TermId.of("NCBIGene:50617");
+        Gene2Genotype ATP6V0A4 = new Gene2Genotype(geneId,symbol);
+        List<TranscriptAnnotation> emptyList = ImmutableList.of();
+        float pathscore=1.0f;
+        float frequency=0.0000001f;
+        ATP6V0A4.addVariant(7,138706689, "G","A",emptyList,"1/1",pathscore,frequency, ClinVarData.ClinSig.PATHOGENIC);
+        // score should be 2 because it is homozgous and maximally pathogenic
+        assertEquals(2.0,ATP6V0A4.getSumOfPathBinScores(),EPSILON);
+    }
+
+
+    @Test
+    void testToString() {
+        // return String.format("%s[%s]: %s",this.symbol,this.geneId.getValue(),varString);
+        String expectedNras="NRAS[NCBIGene:4893]: chr1:114713908A>G null:null:null pathogenicity:0.9 [HETEROZYGOUS]";
+        assertEquals(expectedNras,nrasVar.toString());
     }
 
 }
--- src/test/java/org/monarchinitiative/lr2pg/vcf/SimpleVariantTest.java
+++ src/test/java/org/monarchinitiative/lr2pg/vcf/SimpleVariantTest.java
@@ -1,7 +1,9 @@
 package org.monarchinitiative.lr2pg.vcf;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.monarchinitiative.exomiser.core.model.TranscriptAnnotation;
 import org.monarchinitiative.exomiser.core.model.pathogenicity.ClinVarData;
 
@@ -11,11 +13,36 @@ import java.util.List;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 /**
  * @author <a href="mailto:peter.robinson@jax.org">Peter Robinson</a>
  */
 class SimpleVariantTest {
+    private final static double EPSILON=0.000001;
+    private static SimpleVariant svHet, svHom, svClinVarPath, getSvClinVarLikelyPath;
+    private static SimpleVariant svRareVariant, svMediumRareVariant, svCommonVariant;
+
+    @BeforeAll
+    static void init() {
+        // The following annotation is completely made up, but don't worry it has the right syntax
+        TranscriptAnnotation annot = Mockito.mock(TranscriptAnnotation.class);
+       //ta.getAccession(),ta.getHgvsCdna(),ta.getHgvsProtein()
+        when(annot.getAccession()).thenReturn("uc001lfg.4");
+        when(annot.getHgvsCdna()).thenReturn("c.518A>C");
+        when(annot.getHgvsProtein()).thenReturn("p.(E173A)");
+        when(annot.toString()).thenReturn("uc001lfg.4: c.518A>C p.(E173A) MISSENSE_VARIANT");
+        List<TranscriptAnnotation> annotlist = ImmutableList.of(annot);// not needed
+        String genotypeString="0/1";
+        svHet = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 0.01f, genotypeString);
+        genotypeString="1/1";
+        svHom = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 0.01f, genotypeString);
+        svClinVarPath = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 0.01f, genotypeString, ClinVarData.ClinSig.PATHOGENIC);
+        getSvClinVarLikelyPath = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 0.01f, genotypeString, ClinVarData.ClinSig.LIKELY_PATHOGENIC);
+        svRareVariant = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 0.00f, genotypeString);
+        svMediumRareVariant = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 1.00f, genotypeString);
+        svCommonVariant = new SimpleVariant(2, 23333, "A", "T", annotlist, 0.9f, 5.00f, genotypeString);
+    }
 
 
     @Test
@@ -38,10 +65,7 @@ class SimpleVariantTest {
 
     @Test
     void testHomVariant() {
-        List<TranscriptAnnotation> emptylist = ImmutableList.of();// not needed
-        String genotypeString="1/1";
-        SimpleVariant sv = new SimpleVariant(2, 23333, "A", "T", emptylist, 0.9f, 0.01f, genotypeString);
-        assertEquals(SimpleGenotype.HOMOZYGOUS_ALT, sv.getGtype());
+        assertEquals(SimpleGenotype.HOMOZYGOUS_ALT, svHom.getGtype());
     }
 
     /** Test {@link org.monarchinitiative.lr2pg.vcf.SimpleVariant#isClinVarPathogenic()}  for pathogenic variants. */
@@ -74,6 +98,74 @@ class SimpleVariantTest {
         clinvarsig = ClinVarData.ClinSig.LIKELY_BENIGN;
         sv = new SimpleVariant(2, 23333, "A", "T", emptylist, 0.9f, 0.01f, genotypeString,clinvarsig);
         assertFalse(sv.isClinVarPathogenic());
+    }
+
+    @Test
+    void testXYMchroms() {
+        List<TranscriptAnnotation> emptylist = ImmutableList.of();// not needed
+        String genotypeString="1/1";
+        ClinVarData.ClinSig clinvarsig = ClinVarData.ClinSig.BENIGN;
+        SimpleVariant sv = new SimpleVariant(23, 23333, "A", "T", emptylist, 0.9f, 0.01f, genotypeString,clinvarsig);
+        assertEquals("chrX",sv.getChromosome());
+        sv = new SimpleVariant(24, 23333, "A", "T", emptylist, 0.9f, 0.01f, genotypeString,clinvarsig);
+        assertEquals("chrY",sv.getChromosome());
+        sv = new SimpleVariant(25, 23333, "A", "T", emptylist, 0.9f, 0.01f, genotypeString,clinvarsig);
+        assertEquals("chrM",sv.getChromosome());
+        assertEquals(23333, sv.getPosition());
+    }
+
+    @Test
+    void test_n_a_clinvar() {
+        assertEquals("n/a", svHet.getClinvar());
+        assertEquals("PATHOGENIC", svClinVarPath.getClinvar());
+        assertEquals("LIKELY_PATHOGENIC", getSvClinVarLikelyPath.getClinvar());
+    }
+
+    @Test
+    void toStringTest() {
+        String expected = "chr2:23333A>T uc001lfg.4:c.518A>C:p.(E173A) pathogenicity:0.9 [HOMOZYGOUS_ALT]";
+        assertEquals(expected,svClinVarPath.toString());
+    }
+
+    @Test
+    void testPathScoreRareMediumCommonVar() {
+        // rare variant; path=0.9, freq = 0
+        // frequency score should be 1 * 0.9
+        double expected = 0.9;
+        assertEquals(expected,svRareVariant.getPathogenicity(),EPSILON);
+        // medium variant path=0.9, frequency = 1%
+        // frequency score is 1.13533f - (0.13533f * (float) Math.exp(frequency));
+        //10.9*(1.13533 - (0.13533 * exp(1.0)))
+        //[1] 0.6907184
+        expected = 0.6907184;
+        assertEquals(expected,svMediumRareVariant.getPathogenicity(),EPSILON);
+        // high frequency variant
+        expected = 0.0; // because frequency is 5%
+        assertEquals(expected,svCommonVariant.getPathogenicity(),EPSILON);
+    }
+
+    @Test
+    void testGetRef() {
+        assertEquals("A",svRareVariant.getRef());
+    }
+
+    @Test
+    void testGetAlt() {
+        assertEquals("T",svRareVariant.getAlt());
+    }
+
+    @Test
+    void testGetAnnotation() {
+        List<TranscriptAnnotation> annotlist = svHet.getAnnotationList();
+        assertEquals(1,annotlist.size());
+        TranscriptAnnotation annot = annotlist.get(0);
+        assertEquals("c.518A>C",annot.getHgvsCdna());
+    }
+
+    @Test
+    void testGetFrequency() {
+        double expected=0.01;// 0.01%
+        assertEquals(expected,svHet.getFrequency(),EPSILON);
     }
 
 
