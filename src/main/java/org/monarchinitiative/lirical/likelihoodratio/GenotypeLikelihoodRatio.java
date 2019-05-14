46cbadbf078cd1fb648facd82652715be441fd4d
pnrobinson <peter.robinson@jax.org>
2019-05-13 20:26:15 -0400

    refactoring lambda background

--- src/main/java/org/monarchinitiative/lirical/likelihoodratio/GenotypeLikelihoodRatio.java
+++ src/main/java/org/monarchinitiative/lirical/likelihoodratio/GenotypeLikelihoodRatio.java
@@ -16,30 +16,37 @@ import static org.monarchinitiative.phenol.formats.hpo.HpoModeOfInheritanceTermI
 
 /**
  * This class is responsible for calculating the genotype-based likelihood ratio.
+ *
  * @author <a href="mailto:peter.robinson@jax.org">Peter Robinson</a>
  */
 public class GenotypeLikelihoodRatio {
     private static final Logger logger = LoggerFactory.getLogger(GenotypeLikelihoodRatio.class);
 
-    private static final double DEFAULT_LAMBDA_BACKGROUND=0.1;
+    private static final double DEFAULT_LAMBDA_BACKGROUND = 0.1;
 
-    private static final double EPSILON=1e-5;
+    private static final double EPSILON = 1e-5;
 
-    /** Entrez gene Curie, e.g., NCBIGene:2200; value--corresponding background frequency sum of pathogenic bin variants. */
-    private final Map<TermId,Double> gene2backgroundFrequency;
-    /** This is a Poisson distribution object that is used to help calculate the genotype likelihood ratio for cases
-     * with autosomal recessive inheritance. We can construct this object once and reuse it.*/
+    /**
+     * Entrez gene Curie, e.g., NCBIGene:2200; value--corresponding background frequency sum of pathogenic bin variants.
+     */
+    private final Map<TermId, Double> gene2backgroundFrequency;
+    /**
+     * This is a Poisson distribution object that is used to help calculate the genotype likelihood ratio for cases
+     * with autosomal recessive inheritance. We can construct this object once and reuse it.
+     */
     private final PoissonDistribution recessivePoissonDistribution;
-    /** This is a Poisson distribution object that is used to help calculate the genotype likelihood ratio for cases
-     * with autosomal dominant inheritance. We can construct this object once and reuse it.*/
+    /**
+     * This is a Poisson distribution object that is used to help calculate the genotype likelihood ratio for cases
+     * with autosomal dominant inheritance. We can construct this object once and reuse it.
+     */
     private final PoissonDistribution dominantPoissonDistribution;
 
     /**
      * @param g2background background frequencies of called pathogenic variants in genes.
      */
-    public GenotypeLikelihoodRatio(Map <TermId,Double> g2background) {
-        this.gene2backgroundFrequency=g2background;
-        this.recessivePoissonDistribution =  new PoissonDistribution(2.0);
+    public GenotypeLikelihoodRatio(Map<TermId, Double> g2background) {
+        this.gene2backgroundFrequency = g2background;
+        this.recessivePoissonDistribution = new PoissonDistribution(2.0);
         this.dominantPoissonDistribution = new PoissonDistribution((1.0));
     }
 
@@ -48,6 +55,7 @@ public class GenotypeLikelihoodRatio {
      * intends to represent the probability of missing the variant for technical reasons. We will estimate
      * this probability to be 5%. For autosomal recessive diseases, we will estimate the probability at
      * 5% * 5%.
+     *
      * @param inheritancemodes List of all inheritance modes associated with this disease (usually a single one)
      * @return genotype likelihood ratio for situation where no variant at all was found in a gene
      */
@@ -66,9 +74,10 @@ public class GenotypeLikelihoodRatio {
     /**
      * Calculate the genotype likelihood ratio using lambda_disease=1 for autosomal dominant and lambda_disease=2
      * for autosomal recessive.
-     * @param g2g  {@link Gene2Genotype} object with list of variants in current gene. Can be null if no variants were found in the gene
+     *
+     * @param g2g              {@link Gene2Genotype} object with list of variants in current gene. Can be null if no variants were found in the gene
      * @param inheritancemodes list of modes of inheritance associated with disease being investigated (usually with just one entry).
-     * @param geneId EntrezGene id of the gene we are investigating.
+     * @param geneId           EntrezGene id of the gene we are investigating.
      * @return likelihood ratio of the genotype given the disease/geniId combination
      */
     double evaluateGenotype(Gene2Genotype g2g, List<TermId> inheritancemodes, TermId geneId) {
@@ -86,24 +95,19 @@ public class GenotypeLikelihoodRatio {
             int count = g2g.pathogenicClinVarCount();
             if (inheritancemodes.contains(AUTOSOMAL_RECESSIVE)) {
                 if (count == 2) {
-                    return Math.pow(1000d,2);
+                    return Math.pow(1000d, 2);
                 }
-            } else {
-                if (count == 1) {
-                    return Math.pow(1000d, 1d);
-                }
+            } else { // for all other MoI, including AD, assume that only one ClinVar allele is pathogenic
+                return Math.pow(1000d, 1d);
             }
         }
         double observedWeightedPathogenicVariantCount = g2g.getSumOfPathBinScores();
-        if (observedWeightedPathogenicVariantCount<EPSILON) {
+        if (!g2g.hasPredictedPathogenicVar() || observedWeightedPathogenicVariantCount < EPSILON) {
+            // no identified variant or the pathogenicity score of identified variant is close to zero
             // essentially sam as no identified variant, this should happen rarely if ever.
             return getLRifNoVariantAtAllWasIdentified(inheritancemodes);
         }
 
-
-        if (! g2g.hasPredictedPathogenicVar()) {
-            return getLRifNoVariantAtAllWasIdentified(inheritancemodes);
-        }
         // if we get here then
         // 1. g2g was not null
         // 2. There was at least one observed variant
@@ -111,7 +115,7 @@ public class GenotypeLikelihoodRatio {
         // Therefore, we apply the main algorithm for calculating the LR genotype score.
 
         double lambda_background = this.gene2backgroundFrequency.getOrDefault(geneId, DEFAULT_LAMBDA_BACKGROUND);
-        if (inheritancemodes==null || inheritancemodes.isEmpty()) {
+        if (inheritancemodes == null || inheritancemodes.isEmpty()) {
             // This is probably because the HPO annotation file is incomplete
             logger.warn("No inheritance mode annotation found for geneId {}, reverting to default", geneId.getValue());
             // Add a default dominant mode to avoid not ranking this gene at all
@@ -120,16 +124,16 @@ public class GenotypeLikelihoodRatio {
         // The following is a heuristic to avoid giving genes with a high background count
         // a better score for pathogenic than background -- the best explanation for
         // a gene with high background is that a variant is background (unless variant is ClinVar-path, see above).
-        if (lambda_background>1.0) {
-            lambda_background=Math.min(lambda_background,g2g.pathogenicVariantCount());
+        if (lambda_background > 1.0) {
+            lambda_background = Math.min(lambda_background, g2g.pathogenicVariantCount());
         }
 
         Optional<Double> max = Optional.empty();
         for (TermId inheritanceId : inheritancemodes) {
-            double lambda_disease=1.0;
+            double lambda_disease = 1.0;
             PoissonDistribution pdDisease;
             if (inheritanceId.equals(AUTOSOMAL_RECESSIVE) || inheritanceId.equals(X_LINKED_RECESSIVE)) {
-                lambda_disease=2.0;
+                lambda_disease = 2.0;
                 pdDisease = recessivePoissonDistribution;
             } else {
                 pdDisease = dominantPoissonDistribution;
@@ -139,71 +143,81 @@ public class GenotypeLikelihoodRatio {
             // will take the observed path weighted count to not be more than lambda_disease.
             // this will have the effect of not downweighting these genes
             // the user will have to judge whether one of the variants is truly pathogenic.
-            if (observedWeightedPathogenicVariantCount>lambda_disease && lambda_background < lambda_disease) {
-                double pathogenicVariantCount = Math.max(observedWeightedPathogenicVariantCount,lambda_disease);
-                double D = pdDisease.probability(pathogenicVariantCount);
-                PoissonDistribution pdBackground = new PoissonDistribution(lambda_background);
-                double B = pdBackground.probability(observedWeightedPathogenicVariantCount);
-                return D/B;
+            if (observedWeightedPathogenicVariantCount > lambda_disease ) {
+                if ( lambda_background < lambda_disease) {
+                    double pathogenicVariantCount = Math.max(observedWeightedPathogenicVariantCount, lambda_disease);
+                    double D = pdDisease.probability(pathogenicVariantCount);
+                    PoissonDistribution pdBackground = new PoissonDistribution(lambda_background);
+                    double B = pdBackground.probability(observedWeightedPathogenicVariantCount);
+                    return D / B;
+                } else {
+                    return 1.0;// high lambda background -- probably not interesting so just don't weight it.
+                }
             }
             double D = pdDisease.probability(observedWeightedPathogenicVariantCount);
             PoissonDistribution pdBackground = new PoissonDistribution(lambda_background);
             double B = pdBackground.probability(observedWeightedPathogenicVariantCount);
-            if (B>0 && D>0) {
-                double ratio=D/B;
+            if (B > 0 && D > 0) {
+                double ratio = D / B;
                 if (max.isPresent() && ratio > max.get()) {
-                    max=Optional.of(ratio);
+                    max = Optional.of(ratio);
                 } else if (!max.isPresent()) {
-                    max=Optional.of(ratio);
+                    max = Optional.of(ratio);
                 }
             }
         }
         // We should always have some value for max once we get here but
-        // there is a default value of 0.05^2 to avoid null errors just in case
-       return max.orElse(0.05*0.05);
+        // there is a default value of 0.05^2 to avoid null errors so that
+        // we do not crash if something unexpected occurs.
+        final double DEFAULTVAL = 0.05 * 0.05;
+        return max.orElse(DEFAULTVAL);
     }
 
 
-
-
-
-    /** This method is intended to explain the score that is produced by {@link #evaluateGenotype}, and
+    /**
+     * This method is intended to explain the score that is produced by {@link #evaluateGenotype}, and
      * produces a shoprt summary that can be displayed in the org.monarchinitiative.lirical.output file. It is intended to be used for the
      * best candidates, i.e., those that will be displayed on the org.monarchinitiative.lirical.output page.
+     *
      * @param observedPathogenicVarCount number of variants called to ne pathogenic
-     * @param inheritancemodes List of all inheritance modes associated with this disease (usually has one element,rarely multiple)
-     * @param geneId EntrezGene id of the current gene.
+     * @param inheritancemodes           List of all inheritance modes associated with this disease (usually has one element,rarely multiple)
+     * @param geneId                     EntrezGene id of the current gene.
      * @return short summary of the genotype likelihood ratio score.
      */
     String explainGenotypeScore(double observedPathogenicVarCount, List<TermId> inheritancemodes, TermId geneId) {
         StringBuilder sb = new StringBuilder();
-        double lambda_disease=1.0;
-        if (inheritancemodes!=null && inheritancemodes.size()>0) {
+        double lambda_disease = 1.0;
+        if (inheritancemodes != null && inheritancemodes.size() > 0) {
             TermId tid = inheritancemodes.get(0);
             if (tid.equals(AUTOSOMAL_RECESSIVE) || tid.equals(X_LINKED_RECESSIVE)) {
-                lambda_disease=2.0;
+                lambda_disease = 2.0;
             }
-            if (tid.equals(AUTOSOMAL_DOMINANT)) {sb.append(" Mode of inheritance: autosomal dominant. "); }
-            else if (tid.equals(AUTOSOMAL_RECESSIVE)) {sb.append(" Mode of inheritance: autosomal recessive. "); }
-            else if (tid.equals(X_LINKED_RECESSIVE)) {sb.append(" Mode of inheritance: X-chromosomal recessive. "); }
-            else if (tid.equals(X_LINKED_DOMINANT)) {sb.append(" Mode of inheritance: X-chromosomal recessive. "); }
+            if (tid.equals(AUTOSOMAL_DOMINANT)) {
+                sb.append(" Mode of inheritance: autosomal dominant. ");
+            } else if (tid.equals(AUTOSOMAL_RECESSIVE)) {
+                sb.append(" Mode of inheritance: autosomal recessive. ");
+            } else if (tid.equals(X_LINKED_RECESSIVE)) {
+                sb.append(" Mode of inheritance: X-chromosomal recessive. ");
+            } else if (tid.equals(X_LINKED_DOMINANT)) {
+                sb.append(" Mode of inheritance: X-chromosomal recessive. ");
+            }
         }
         double lambda_background = this.gene2backgroundFrequency.getOrDefault(geneId, DEFAULT_LAMBDA_BACKGROUND);
         sb.append(String.format("Observed weighted pathogenic variant count: %.2f. &lambda;<sub>disease</sub>=%d. &lambda;<sub>background</sub>=%.4f. ",
-                observedPathogenicVarCount,(int)lambda_disease,lambda_background));
+                observedPathogenicVarCount, (int) lambda_disease, lambda_background));
         double D;
-        if (observedPathogenicVarCount<EPSILON) {
-            D=0.05; // heuristic--chance of zero variants given this is disease is 5%
+        if (observedPathogenicVarCount < EPSILON) {
+            D = 0.05; // heuristic--chance of zero variants given this is disease is 5%
         } else {
             PoissonDistribution pdDisease = new PoissonDistribution(lambda_disease);
             D = pdDisease.probability(observedPathogenicVarCount);
         }
         PoissonDistribution pdBackground = new PoissonDistribution(lambda_background);
         double B = pdBackground.probability(observedPathogenicVarCount);
-        sb.append(String.format("P(G|D)=%.4f. P(G|&#172;D)=%.4f",D,B));
-        if (B>0 && D>0) {
-            double r=Math.log10(D/B);
-            sb.append(String.format(". log<sub>10</sub>(LR): %.2f.",r));
+        sb.append(String.format("P(G|D)=%.4f. P(G|&#172;D)=%.4f", D, B));
+        if (B > 0 && D > 0) {
+            double r = Math.log10(D / B);
+            sb.append(String.format(". log<sub>10</sub>(LR): %.2f.", r));
         }
         return sb.toString();
     }
