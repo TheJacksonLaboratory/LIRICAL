75a417b3e3ec480d594071fb058cff480aebe146
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-07-01 15:53:50 -0400

    Catch `PhenopacketImportException` in `PhenopacketCommand`s.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- lirical-beta/src/main/java/org/monarchinitiative/lirical/beta/cmd/PhenopacketCommand.java
+++ lirical-beta/src/main/java/org/monarchinitiative/lirical/beta/cmd/PhenopacketCommand.java
@@ -6,6 +6,7 @@ import org.monarchinitiative.lirical.core.analysis.LiricalParseException;
 import org.monarchinitiative.lirical.core.model.GenesAndGenotypes;
 import org.monarchinitiative.lirical.core.service.HpoTermSanitizer;
 import org.monarchinitiative.lirical.io.analysis.PhenopacketData;
+import org.monarchinitiative.lirical.io.analysis.PhenopacketImportException;
 import org.monarchinitiative.lirical.io.analysis.PhenopacketImporter;
 import org.monarchinitiative.lirical.io.analysis.PhenopacketImporters;
 import org.monarchinitiative.phenol.ontology.data.TermId;
@@ -51,7 +52,7 @@ public class PhenopacketCommand extends BaseSquirlsAwareCommand {
             PhenopacketImporter v2 = PhenopacketImporters.v2();
             data = v2.read(is);
             LOGGER.info("Success!");
-        } catch (IOException e) {
+        } catch (PhenopacketImportException | IOException e) {
             LOGGER.info("Unable to parse as v2 phenopacket, trying v1.");
         }
 
@@ -59,7 +60,7 @@ public class PhenopacketCommand extends BaseSquirlsAwareCommand {
             try (InputStream is = Files.newInputStream(phenopacketPath)) {
                 PhenopacketImporter v1 = PhenopacketImporters.v1();
                 data = v1.read(is);
-            } catch (IOException e) {
+            } catch (PhenopacketImportException | IOException e) {
                 LOGGER.info("Unable to parser as v1 phenopacket.");
                 throw new LiricalParseException("Unable to parse phenopacket from " + phenopacketPath.toAbsolutePath());
             }
