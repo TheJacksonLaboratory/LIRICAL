e601d90c02b188d9ecaf18065aebf9db3103bc70
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-01-31 16:53:52 +0100

    Add `targetDiseases` analysis option.

--- lirical-core/src/main/java/org/monarchinitiative/lirical/core/analysis/AnalysisOptionsDefault.java
+++ lirical-core/src/main/java/org/monarchinitiative/lirical/core/analysis/AnalysisOptionsDefault.java
@@ -4,13 +4,16 @@ import org.monarchinitiative.lirical.core.analysis.probability.PretestDiseasePro
 import org.monarchinitiative.lirical.core.model.GenomeBuild;
 import org.monarchinitiative.lirical.core.model.TranscriptDatabase;
 import org.monarchinitiative.phenol.annotations.io.hpo.DiseaseDatabase;
+import org.monarchinitiative.phenol.ontology.data.TermId;
 
+import java.util.Collection;
 import java.util.Set;
 
 record AnalysisOptionsDefault(
         GenomeBuild genomeBuild,
         TranscriptDatabase transcriptDatabase,
         Set<DiseaseDatabase> diseaseDatabases,
+        Collection<TermId> targetDiseases,
         float variantDeleteriousnessThreshold,
         double defaultVariantBackgroundFrequency,
         boolean useStrictPenalties,
